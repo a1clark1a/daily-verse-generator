6a520afa148ff8cf26e0b43c334d09d81b0aa4ee
Clark <a1clark1a@gmail.com>
2025-10-16 16:06:21 -0700

    images and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,64 @@
-import { GradientBackgroundCon } from "@/components/quoteGenerator/QuoteGeneratorElements";
-import { Flex, Heading } from "@chakra-ui/react";
+"use client";
+
+import { useState } from "react";
+
+import Image from "next/image";
+import Link from "next/link";
+
+import { Link as ChakraLink, Text, Icon } from "@chakra-ui/react";
+import { HiHeart } from "react-icons/hi";
+
+import {
+  FooterContainer,
+  GradientBackgroundCon,
+} from "@/components/quoteGenerator/QuoteGeneratorElements";
+
+// images
+import cloud1 from "@/assets/cloud-and-thunder.png";
+import cloud2 from "@/assets/cloudy-weather.png";
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<number | null>(0);
+
   return (
     <GradientBackgroundCon>
-      <Flex
-        as="main"
-        direction="column"
-        align="center"
-        justify="center"
-        h="100%"
-      >
-        <Heading color="white">Inspirational Quote</Heading>
-      </Flex>
+      <Image src={cloud1} alt="cloudImage1" priority height={300} />
+
+      <Image
+        src={cloud2}
+        alt="cloudImage2"
+        priority
+        height={300}
+        style={{
+          position: "fixed",
+          zIndex: 1,
+          right: "-120px",
+          bottom: "10px",
+        }}
+      />
+
+      <FooterContainer>
+        <>
+          Quotes Generated: {numberOfQuotes}
+          <br />
+          <Text>
+            Developed with{" "}
+            <Icon size="lg" color="red.700">
+              <HiHeart />
+            </Icon>
+            :{" "}
+            <ChakraLink
+              variant="underline"
+              href="https://acperfecto.vercel.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Anthony Clark Perfecto
+            </ChakraLink>{" "}
+            @{new Date().getFullYear()}
+          </Text>
+        </>
+      </FooterContainer>
     </GradientBackgroundCon>
   );
 }
